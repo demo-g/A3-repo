e9689062bd2981964661b520eab0f8b98c183583
Hacker <Hacker@DESKTOP-910EUPM>
2020-03-26 15:43:59 +0530

    commit

---
+++ Demo1/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.boot;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new MainClass().print();
+	}
+	void print()
+	{
+		System.out.println("This is main class");
+	}
+}
