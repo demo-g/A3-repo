7d421eefafe2e7811d3038ddca39d796a2136a1a
Suryakanta Behera <beherasuryakanta18@gmail.com>
2020-03-27 16:36:16 +0530

    Sales Class Updated

--- Demo1/src/main/java/org/cap/model/SalesClass.java
+++ Demo1/src/main/java/org/cap/model/SalesClass.java
@@ -2,9 +2,19 @@ package org.cap.model;
 
 public class SalesClass {
 
-	
+	int arr[]= {10,20,50,30};
 	public void sales()
 	{
 		System.out.println("This is SalesClass");
+	}
+
+	public void print()
+	{
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			sum+=arr[i];
+			System.out.println(arr[i]);
+		}
 	}
 }
