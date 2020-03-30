9d8db5e61493ea042ca0a76a372073aa5b3cb628
Suryakanta Behera <beherasuryakanta18@gmail.com>
2020-03-30 09:15:29 +0530

    Sales Class updated

--- Demo1/src/main/java/org/cap/model/SalesClass.java
+++ Demo1/src/main/java/org/cap/model/SalesClass.java
@@ -10,12 +10,12 @@ public class SalesClass {
 
 	public void print()
 	{
-<<<<<<< HEAD
+
 		for(int num:arr)
 				System.out.println(num);
 		System.out.println("This is developer 1");
 System.out.println("Enhanced for loop");
-=======
+
 		int sum=0;
 		for(int i=0;i<arr.length;i++)
 		{
@@ -24,6 +24,6 @@ System.out.println("Enhanced for loop");
 		}
 		System.out.println("THis is developer 2");
 		System.out.println("Normal For loop ");		
->>>>>>> branch 'master' of https://github.com/demo-g/A3-repo.git
+
 	}
 }
